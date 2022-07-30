c1c6648d85a0c55c7708f8abee9f29ecded6c659
benny <benny@benny-optiplex-7060>
2022-07-30 07:19:35 +0100

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,77 @@
 
 <template>
   <main class="container mt-5" v-if="!is_recording">
-    <div class="row justify-content-end mt-3">
-      <div class="col-auto me-auto">
-        <div class="lh-1">
-          <small>Snapbyte > My Recordings</small>
-          <h1 class="h6 mb-0 mt-3 lh-1">My Recordings <strong>25</strong></h1>
-        </div>
-      </div>
-      <div class="col-auto">
-        <button type="button" class="btn rounded-pill mx-4 btn-light btn-outline-dark">By Date</button>
-        <button type="button" class="btn rounded-pill mx-4 btn-outline-dark">Add Filter</button>
-        <button type="button" class="btn rounded-pill mx-4 btn-info">New Request</button>
-        <button type="button" data-bs-toggle="modal" data-bs-target="#record" class="btn rounded-pill mx-4 btn-danger">Start Recording</button>
-      </div>
-
-    </div>
-
-    <div class="row mt-5">
-      <table class="table table-borderless">
-        <thead>
-        <tr>
-          <th scope="col" style="width: 10%">Recordings</th>
-          <th scope="col" style="width: 40%">Title</th>
-          <th scope="col" style="width: 15%"></th>
-          <th scope="col">Views</th>
-          <th scope="col">Size</th>
-          <th scope="col">Last Modified</th>
-          <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(i, index) in recordings" :key="index">
-          <th scope="row">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="100" height="65" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-          </th>
-          <td>
-            <p>{{  i.title }}<br/>
-              <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias aperiam cupiditate deserunt dolorum eligendi hic ipsum itaque,</small>
-            </p>
-          </td>
-          <td></td>
-          <td>{{  i.views }}</td>
-          <td>{{ i.size }}</td>
-          <td>{{ i.last_modified }}</td>
-          <td>...</td>
-        </tr>
-
-        </tbody>
-      </table>
-    </div>
-
-
-    <!-- Start Recording Modal -->
-    <div class="modal fade" id="record" tabindex="-1" aria-labelledby="record" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="record">New Recording</h5>
-            <button type="button" class="btn-close" id="close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <template v-if="!loading">
+      <div class="row justify-content-end mt-3">
+        <div class="col-auto me-auto">
+          <div class="lh-1">
+            <small>Snapbyte > My Recordings</small>
+            <h1 class="h6 mb-0 mt-3 lh-1">My Recordings <strong>25</strong></h1>
           </div>
-          <div class="modal-body">
-            <div class="row">
-              <div class="col-12">
-                <p>Save the recording in</p>
-                <select class="form-select">
-                  <option selected>Select a project</option>
-                </select>
-              </div>
+        </div>
+        <div class="col-auto">
+          <button type="button" class="btn rounded-pill mx-4 btn-light btn-outline-dark">By Date</button>
+          <button type="button" class="btn rounded-pill mx-4 btn-outline-dark">Add Filter</button>
+          <button type="button" class="btn rounded-pill mx-4 btn-info">New Request</button>
+          <button type="button" data-bs-toggle="modal" data-bs-target="#record" class="btn rounded-pill mx-4 btn-danger">Start Recording</button>
+        </div>
 
+      </div>
+
+      <div class="row mt-5">
+        <table class="table table-borderless">
+          <thead>
+          <tr>
+            <th scope="col" style="width: 10%">Recordings</th>
+            <th scope="col" style="width: 40%">Title</th>
+            <th scope="col" style="width: 15%"></th>
+            <th scope="col">Views</th>
+            <th scope="col">Size</th>
+            <th scope="col">Last Modified</th>
+            <th scope="col"></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(i, index) in recordings" :key="index">
+            <th scope="row">
+              <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="100" height="65" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+            </th>
+            <td>
+              <p>{{  i.title }}<br/>
+                <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias aperiam cupiditate deserunt dolorum eligendi hic ipsum itaque,</small>
+              </p>
+            </td>
+            <td></td>
+            <td>{{  i.views }}</td>
+            <td>{{ i.size }}</td>
+            <td>{{ i.last_modified }}</td>
+            <td>...</td>
+          </tr>
+
+          </tbody>
+        </table>
+      </div>
+
+
+      <!-- Start Recording Modal -->
+      <div class="modal fade" id="record" tabindex="-1" aria-labelledby="record" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="record">New Recording</h5>
+              <button type="button" class="btn-close" id="close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="row mt-3">
+            <div class="modal-body">
+              <div class="row">
+                <div class="col-12">
+                  <p>Save the recording in</p>
+                  <select class="form-select">
+                    <option selected>Select a project</option>
+                  </select>
+                </div>
+
+              </div>
+              <div class="row mt-3">
                 <div class="col-auto me-auto">
                   <div class="lh-1">
                     <h1 class="h6">Record Screen</h1>
@@ -78,11 +79,11 @@
                 </div>
                 <div class="col-auto">
                   <div class="form-check form-switch">
-                  <input v-model="screen" class="form-check-input" type="checkbox" id="screen">
+                    <input v-model="screen" class="form-check-input" type="checkbox" id="screen">
                   </div>
                 </div>
-            </div>
-            <div class="row mt-3">
+              </div>
+              <div class="row mt-3">
                 <div class="col-auto me-auto">
                   <div class="lh-1">
                     <h1 class="h6">Record Camera</h1>
@@ -90,11 +91,11 @@
                 </div>
                 <div class="col-auto">
                   <div class="form-check form-switch">
-                  <input v-model="video" class="form-check-input" type="checkbox" id="video">
+                    <input v-model="video" class="form-check-input" type="checkbox" id="video">
                   </div>
                 </div>
-            </div>
-            <div class="row mt-3">
+              </div>
+              <div class="row mt-3">
                 <div class="col-auto me-auto">
                   <div class="lh-1">
                     <h1 class="h6">Record Mic</h1>
@@ -102,18 +103,20 @@
                 </div>
                 <div class="col-auto">
                   <div class="form-check form-switch">
-                  <input v-model="audio" class="form-check-input" type="checkbox" id="audio">
+                    <input v-model="audio" class="form-check-input" type="checkbox" id="audio">
                   </div>
                 </div>
-            </div>
+              </div>
 
-          </div>
-          <div class="modal-footer text-center justify-content-center">
-            <button @click="startRecord" type="button" class="btn rounded-pill btn-primary text-center">Start Recording</button>
+            </div>
+            <div class="modal-footer text-center justify-content-center">
+              <button @click="startRecord" type="button" class="btn rounded-pill btn-primary text-center">Start Recording</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+    </template>
 
   </main>
 
